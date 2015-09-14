67fb6ef072629cf4edd745dac6575c701831c983
Gerred Dillon <hello@gerred.com>
2015-09-14 17:25:23 -0600

    push failing test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ConcoursePhoenixExample.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Phoenx"
   end
 end
